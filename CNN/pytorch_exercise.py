0fdf6459288cdf5e53f1d0f77533d3707ec8df8e
papadate <zhaozepeng@pc-201-245.customer.ask4.lan>
2023-11-08 23:21:43 +0000

    pytorch

--- CNN/pytorch_exercise.py
+++ CNN/pytorch_exercise.py
@@ -1,1 +1,12 @@
 import torch
+import numpy as np
+
+matrix_np = np.random.random((3, 4))
+print(matrix_np.shape)
+matrix_torch = torch.rand(3, 4)
+print(matrix_torch.size())
+x = torch.tensor([1, 2, 3])
+# size() 和 shape 是一个效果。 numpy 里面也是 .shape
+print(x.size())
+print(x.shape)
+
