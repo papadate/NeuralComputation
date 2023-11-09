3f5c8798c6b2f05127df262c90a071281414d2d1
papadate <zhaozepeng@Zepengs-MacBook-Pro.local>
2023-11-09 15:39:22 +0000

    upload error model

--- CNN/SimpleModel.py
+++ CNN/SimpleModel.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 # 引入优化方法库
 import torch.optim as optim
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class SimpleModel(nn.Module):
@@ -37,11 +38,37 @@ x = np.random.rand(100, 1)
 # 定义计算y 的函数， 1 + 2倍的x + 一些干扰
 y = 1 + (2 * x) + (0.1 * np.random.randn(100, 1))
 
+# 对应 100 个样本的数据
+index = np.arange(100)
+print(index)
+print("打乱数组中...")
+np.random.shuffle(index)
+print(index)
 
+# 分割数据集
+train_index = index[:80] # 从 0 - 79
+validation_index = index[80:] # 从 79 - 99
 
+# 分配数据到 训练集 和 验证集
+x_train = x[train_index]
+y_train = y[train_index]
+print("训练集数量x-{}, y-{}".format(x_train.size, y_train.size))
+x_val = x[validation_index]
+y_val = y[validation_index]
+print("验证集数量x-{}, y-{}".format(x_val.size, y_val.size))
 
+# 转化numpy矩阵 到 torch 矩阵
+x_train_torch = torch.from_numpy(x_train).float()
+y_train_torch = torch.from_numpy(x_train).float()
+x_val_torch = torch.from_numpy(x_val)
+y_val_torch = torch.from_numpy(y_val)
 
-
+# 画图：
+# 创建一张图，1行两列（同一行里2张图）
+fig, axs = plt.subplots(nrows=1, ncols=2)
+axs[0].scatter(x_train, y_train)
+axs[1].scatter(x_val, y_val)
+# plt.show()
 
 # 设置固定随机数生成器，保证每次运时随机数一至
 torch.manual_seed(42)
@@ -59,15 +86,28 @@ print("\n")
 print("参数 训练之前：\n {}".format(model.state_dict()))
 
 # 设置超参数：
-learning_rate = 1e-1  # 0,1
-epoches = 100 # 训练数据循环次数
+learning_rate = 1e-1  # 0.1
+epochs = 100  # 训练数据循环次数
 loss_fn = nn.MSELoss(reduction='mean')
 # 损失函数 MSE 用均值
 optimiser = optim.SGD(model.parameters(), lr=learning_rate)
 # 使用SGD优化参数， lr-学习率是learning-rate, 提前设置好的
 # 这里SGD将会优化参数 a 和 b
 
-for epoch in range(epoches):
+for epoch in range(epochs):
+    # 设置为训练模式
     model.train()
+    # 在每次epoch训练时，梯度归零，防止梯度积累
     optimiser.zero_grad()
-    y_prediction = model()
+    # 前向传播， 用目前的模型和输入计算出 预测y
+    y_prediction = model(x_train_torch.to(device))
+    # 通过定义好的损失函数来计算损失 你的标准答案和模型预测值
+    loss = loss_fn(y_train_torch.to(device), y_prediction)
+    # print(loss)
+    # 利用loss进行反向传播
+    loss.backward()
+    # 之前定义的优化方法，step() 开始利用梯度和学习率去更新参数
+    optimiser.step()
+    # print(model.state_dict())
+
+print("After Training, the parameter will be: \n {}". format(model.state_dict()))
