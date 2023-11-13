8a28eb0975107505b070f446856a7e7b220dc3fc
papadate <zhaozepeng038@gmail.com>
2023-11-13 22:09:52 +0000

    model update

--- CNN/nerualnetwork.py
+++ CNN/nerualnetwork.py
@@ -14,7 +14,7 @@ col - dimensions
 sample = number of points each class (N) * number of class (K)
 """
 X = np.zeros((N*K, D))
-y = np.zeros(N*K, dtype='unit8')
+y = np.zeros(N*K, dtype='uint8')
 
 # 循环每一个类
 for j in range(K):
@@ -28,6 +28,26 @@ for j in range(K):
     """
     ix = range(N * j, N * (j + 1))
     """
+    np.linspace()
+    可用参数：
+    start 起始值
+    stop 结束值
+    num 中间要生成的等间隔样本 默认 50个
+    dtype 输出数组类型
+    axis 沿着哪个轴填充数组 默认 0
+    0 -> 列的方向 竖着的
+    1 -> 行的方向 横着的
     
+    此代码行意义：
+    从 0.0 开始 到 1 等间隔分割N个
     """
-    r = np.linspace(0.0, 1, N)
+    r = np.linspace(0.0, 1, N) # 半径的数组
+    #   角度数组                              随机噪声
+    t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * 0.2
+    # 极坐标系 转换成 直接坐标系 （x， y）
+    X[ix] = np.c_[r * np.cos(t), r * np.sin(t)]
+    # 对 ix 数据 进行分类
+    y[ix] = j
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=20, cmap=plt.cm.Spectral)
+plt.show()
