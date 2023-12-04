06ad3b828b681b45119b35d0195c011105e2f152
papadate <zhaozepeng038@gmail.com>
2023-12-04 00:03:42 +0000

    chinese log

--- Pytorch/torch_practice.py
+++ Pytorch/torch_practice.py
@@ -2,16 +2,16 @@
 import numpy as np
 import torch
 
-print("Construct a 3*4 randomly initialised matrix")
+print("创建一个 3*4 的 tensor 矩阵")
 matrix = torch.rand(3,4)
 print(matrix)
 print("matrix's size is :", matrix.size(), "\n")
 
-print("matrix.shape also works!")
+print("matrix.shape 也可以获取tensor matrix 的尺寸!")
 print("matrix's size is :", matrix.shape, "\n")
 
-print("You can construct a tensor directly from data")
-print("ie. A List of Object")
+print("你可以直接从数据中创建tensor")
+print("比如： A List of Object")
 print("[3, 4.5]")
 list = [3, 4.5]
 matrix = torch.tensor(list)
