baaf200d1d7b665c2cd3474701ffc36e807da184
papadate <zhaozepeng038@gmail.com>
2023-12-04 12:07:18 +0000

    pytorch

--- Pytorch/welcome.py
+++ Pytorch/welcome.py
@@ -3,8 +3,10 @@
 # 按 ⌃R 执行或将其替换为您的代码。
 # 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
 
-
 str = ("这是一个欢迎文件！"
        "本项目用于学习pytorch\n"
        "‘env’文件夹是一个本地安装包虚拟环境")
 print(str)
+
+import torch_practice
+import mps_practice
