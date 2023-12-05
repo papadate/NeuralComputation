ba0e3923a6bd7850bae52f9bb397ef8417db2f3d
papadate <zhaozepeng038@gmail.com>
2023-12-05 11:38:54 +0000

    pytorch

--- Pytorch/model/resource/dataset.py
+++ Pytorch/model/resource/dataset.py
@@ -5,3 +5,6 @@ import torch
 np.random.seed(42)
 # 因为前面已经固定了随机种子，所以随机出来的数值永远每次执行任务都是一致的
 data_x = np.random.rand(100, 1)
+# 生成 对应输出 y 的值
+# 公式：y = b(b=1) + w(w=2) * x + (noise)
+data_y = 1 + 2 * data_x + 0.1 * np.random.randn(100, 1)
