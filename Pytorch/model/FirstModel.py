0bf3858fb79ea80fd161444f8ec8f63c91a44b9d
papadate <zhaozepeng038@gmail.com>
2023-12-05 20:18:56 +0000

    torch

--- Pytorch/model/FirstModel.py
+++ Pytorch/model/FirstModel.py
@@ -2,8 +2,8 @@ import torch
 
 
 class FirstModel(torch.nn.Module):
-    def __int__(self):
-        super().__int__()
+    def __init__(self):
+        super(FirstModel, self).__init__()
         # 定义参数
         self.b = torch.nn.Parameter(torch.randn(1).float())  # 生成一个随机标量
         self.w = torch.nn.Parameter(torch.randn(1).float())  # 生成一个随机标量
@@ -11,3 +11,16 @@ class FirstModel(torch.nn.Module):
     def forward(self, x):
         # formula: W * X + b
         return (self.w * x) + self.b
+
+
+import torch.optim as optim
+import model.resource.mps_checker as checker
+
+torch.manual_seed(42)
+
+device = ""
+model = checker.check(FirstModel())
+
+print("分析模型参数：")
+for i in iter(model.parameters()):
+    print(i)
---
+++ Pytorch/model/resource/mps_checker.py
@@ -0,0 +1,13 @@
+import torch
+
+
+def check(modelClass):
+    if torch.backends.mps.is_available():
+        device = "mps"
+        print("检测到计算机可以使用：{}\n".format(device))
+        model = modelClass.to(device)
+        return model
+    else:
+        print("检测到计算机仅有CPU")
+        model = modelClass
+        return model
