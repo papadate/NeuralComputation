707805b6bf6de89a0da08332f0b82ce033c69dba
papadate <zhaozepeng038@gmail.com>
2023-12-23 07:08:52 +0800

    autoencoder

--- Autoencoder/main.py
+++ Autoencoder/main.py
@@ -2,6 +2,7 @@ import numpy as np
 from utils.data_utils import get_mnist
 from utils.plotting import plot_grid_of_images
 import matplotlib.pyplot as plt
+from utils.plotting import plot_train_progress_1, plot_grids_of_images
 
 # 常量参数
 Data_direction = './data/mnist'
@@ -46,7 +47,7 @@ print('测试集内 类别为: ')
 print(np.unique(sets[3]))
 
 i = 100
-plot_grid_of_images(train_images[i:i+900], n_imgs_per_row=30)
+plot_grid_of_images(train_images[i:i+100], n_imgs_per_row=10)
 plt.show()
 
 # 此步操作，是用来把2D矩阵拉直变成1D向量
@@ -56,3 +57,19 @@ test_images_flat = test_images.reshape([test_images.shape[0], -1])
 print("numpy array 前后变化：")
 print('1 -> {}'.format(train_images.shape))
 print('2 -> {}'.format(train_images_flat.shape))
+
+# 数据分割函数
+def get_random_batch(train_images, train_labels, batch_size, random_generator):
+    indices = range(0, batch_size)
+    indices = random_generator.randint(low=0,
+                                       high=train_images[0],
+                                       size=batch_size,
+                                       dtype='int32')
+    train_images_batch = train_images[indices]
+
+    # 标签集合分流： 监督训练有标签， 无监督训练无标签
+    if train_labels is not None:
+        train_labels_batch = train_labels[indices]
+    else:
+        train_labels_batch = None
+    return train_images_batch, train_labels_batch
